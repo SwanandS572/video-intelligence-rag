74512cf4351bea265f1db5a9349b0ea1e30f8ea5
SwanandS572 <swanandsinnarkar7@gmail.com>
2026-04-10 20:39:59 +0530

    Initial commit

---
+++ processing_incoming.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity 
+import joblib
+import requests
+
+def create_embeddings(text_list):
+    r = requests.post("http://localhost:11434/api/embed", json={
+        "model": "bge-m3",
+        "input": text_list
+    })
+
+    embedding = r.json()['embeddings']
+    return embedding
+
+df = joblib.load('embeddings.joblib')
+incoming_query = input("Ask a Question: ")
+question_embedding = create_embeddings([incoming_query])[0]
+# print(question_embedding)
+
+similarities = cosine_similarity(np.vstack(df['embedding']), [question_embedding]).flatten()
+print(similarities) 
+top_results = 3
+max_index = similarities.argsort()[::-1][0:top_results]
+print(max_index)
+new_df = df.loc[max_index]
+# print(new_df[["title", "number", "text"]])
+print(new_df[["Video_title", "Video_num", "text"]])  # ✅ Correct names
+
+for index,item in new_df,iterrows():
+    print(index,item["Video_title"],item["Video_num"],item["text"],item["start"],item["end"])
---
+++ read_chunks.py
@@ -0,0 +1,46 @@
+import requests
+import os
+import json
+import pandas as pd
+import numpy as np
+import joblib
+from sklearn.metrics.pairwise import cosine_similarity
+
+def create_embeddings(text_list):
+    r = requests.post("http://localhost:11434/api/embed", json={
+        "model": "bge-m3",
+        "input": text_list
+    })
+
+    embedding = r.json()['embeddings']
+    return embedding
+
+# a=create_embeddings("Cat sat on the mat")
+# print(a)
+
+jsons = os.listdir("jsons") # List all the jsons
+# print(jsons)
+mydicts=[]
+chunk_id = 0
+
+for j in jsons:
+    with open(f"jsons/{j}") as f:
+        content = json.load(f)
+    print(f"Creating Embeddings for {j} file")
+    embeddings = create_embeddings([c['text'] for c in content['chunks']])
+    for i, chunk in enumerate(content['chunks']):
+        chunk['chunk_id'] = chunk_id
+        chunk['embedding'] = embeddings[i]
+        chunk_id += 1
+        mydicts.append(chunk)
+    #     if(i==3):
+    #         break
+    # break
+
+# print(mydicts)
+
+df = pd.DataFrame.from_records(mydicts)
+# print(df)
+joblib.dump(df, 'embeddings.joblib')
+
+
